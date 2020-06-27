0e4c92af5602de4c0cb137bb34dcab4bda0e1a8e
XiaoPan <2489964425@qq.com>
2020-06-27 15:28:16 +0800

    权限管理开发完成

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/users/Users.vue'
 import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
+import Roles from '../components/power/roles.vue'
 
 Vue.use(VueRouter)
 
